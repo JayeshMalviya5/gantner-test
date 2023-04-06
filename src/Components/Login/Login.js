52333e1fbb7e99d2309cb11d5007088ea7724e4a
jayeshmalviya5 <jayeshmalviya10@gmail.com>
2023-04-06 18:39:40 +0530

    hkhklhlk

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -17,28 +17,31 @@ function Login() {
     setDetails(newDetails);
   };
 
-
   const handleLogin = async (event) => {
     // console.log(data)
     event.preventDefault();
-    const data = `"${details.userName},${details.password}"`
-    console.log(data.toString)
-    console.log(data)
-    const local_data = localStorage.getItem("user")
-    console.log(local_data)
-    if(data.toString() == local_data){
-      navigate("/landing")
-    }else{
-      alert("enter correct credentials")
+    const data = `"${details.userName},${details.password}"`;
+    console.log(data.toString);
+    console.log(data);
+    const local_data = localStorage.getItem("user");
+    console.log(local_data);
+    if (data.toString() == local_data) {
+      navigate("/landing");
+    } else {
+      alert("enter correct credentials");
     }
-  }
- 
+  };
 
   return (
     <>
       <div className="Scontainer">
         <div className="SloginContainer">
-          <div className="login" style={{fontWeight: 800, marginBottom:"10px"}}>Login</div>
+          <div
+            className="login"
+            style={{ fontWeight: 800, marginBottom: "10px" }}
+          >
+            Login
+          </div>
           <div className="SuserName">
             <label htmlFor="">EmailID</label>
             <div className="Sinput">
@@ -65,11 +68,11 @@ function Login() {
             Login
           </button>
           <div>
-          <Link to="/signup">Click to Go on Signup page</Link>
+            <Link to="/signup">Click to Go on Signup page</Link>
           </div>
         </div>
       </div>
-      </>
+    </>
   );
 }
 
